4723bdf996e5f3d20116e00672a02b05e09437eb
ashwinikb <ashwini.kbasavaraju@gmail.com>
2017-12-30 10:46:08 -0800

    Finding number closest to zero in an Array.

---
+++ src/main/java/com/github/ashwinikb/code/ClosestToZero.java
@@ -0,0 +1,54 @@
+package com.github.ashwinikb.code;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Implement this method to return the number in the array that is closest to
+ * zero. If there are two equally close to zero elements like 2 and -2 -
+ * consider the positive element to be "closer" to zero.
+ * 
+ * @author Ashwini KB
+ * 
+ */
+
+public class ClosestToZero {
+	public static int getClosestToZero(int[] a) {
+
+		Arrays.sort(a);
+
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > 0) {
+				if (i == 0) {
+					return a[i];
+				}
+
+				int diff1 = a[i] - 0;
+
+				int diff2;
+
+				if (a[i - 1] != 0) {
+					diff2 = Math.abs(a[i - 1]) - 0;
+				} else {
+
+					if (i >= 2) {
+						return a[i - 2];
+					} else {
+						return a[i];
+					}
+
+				}
+
+				if (diff1 > diff2) {
+					return a[i - 1];
+				} else {
+					return a[i];
+				}
+
+			}
+		}
+
+		return 0;
+
+	}
+}
---
+++ src/test/java/com/github/ashwinikb/code/ClosestToZeroTest.java
@@ -0,0 +1,46 @@
+package com.github.ashwinikb.code;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ClosestToZeroTest {
+	
+	@Test
+	public void test() {
+		int a[] = {10,2,-2,-1,5};
+		int result = ClosestToZero.getClosestToZero(a);
+		assertEquals(result,-1);
+		System.out.println(result);
+		
+	}
+	
+	@Test
+	public void test2() {
+		int a[] = {5,20,10,2};
+		int result = ClosestToZero.getClosestToZero(a);
+		assertEquals(result,2);
+		System.out.println(result);
+		
+	}
+	@Test
+	public void test3() {
+		int a[] = {5,3,0,-1,-2};
+		int result = ClosestToZero.getClosestToZero(a);
+		assertEquals(result,-1);
+		System.out.println(result);
+		
+	}
+	
+	@Test
+	public void test4() {
+		int a[] = {5,3,0};
+		int result = ClosestToZero.getClosestToZero(a);
+		assertEquals(result,3);
+		System.out.println(result);
+		
+	}
+
+
+
+}
