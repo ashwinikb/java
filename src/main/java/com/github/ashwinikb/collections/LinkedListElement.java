8d38ea472e1b85fb48e3a89faef3e5a5410051a2
ashwinikb <ashwini.kbasavaraju@gmail.com>
2018-05-11 14:55:52 -0700

    Finding Nth element from Last Node of Linked LIst

---
+++ src/main/java/com/github/ashwinikb/collections/LinkedListElement.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.github.ashwinikb.collections;
+
+/**
+ * @author ashwini
+ *
+ */
+public class LinkedListElement {
+	private static Node head;
+
+	public class Node {
+		int data;
+		Node next;
+
+		Node(int data) {
+			this.data = data;
+		}
+	}
+
+	public void addElement(Node node) {
+		if (head == null) {
+			head = node;
+		} else {
+			Node temp = head;
+			while (temp.next != null)
+				temp = temp.next;
+			temp.next = node;
+		}
+	}
+
+	public void printList() {
+		Node temp = head;
+		while (temp.next != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+
+	public Node nthElementLastNode(Node head, int n) {
+		Node fastPointer = head;
+		Node slowPointer = head;
+
+		for (int i = 0; i < n; i++) {
+			fastPointer = fastPointer.next;
+		}
+		while (fastPointer != null) {
+			fastPointer = fastPointer.next;
+			slowPointer = slowPointer.next;
+		}
+		return slowPointer;
+	}
+
+}
