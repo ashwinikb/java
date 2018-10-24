ebf443c5e3cc2e2c105b7a0da86e2e25ffaed689
ashwinikb <ashwini.kbasavaraju@gmail.com>
2018-10-23 22:49:02 -0700

    Checking whether a given number is prime

---
+++ src/main/java/com/github/ashwinikb/code/PrimeNumber.java
@@ -0,0 +1,16 @@
+package com.example.ashwinikb;
+
+public class PrimeNumber {
+	public static boolean prime(int num) {
+	
+		for(int i = 2; i<num/2 ; i++ ) {
+			if((num%i) == 0) {
+				return false;
+			}
+		}
+		
+		return true;
+		
+	}
+
+}
