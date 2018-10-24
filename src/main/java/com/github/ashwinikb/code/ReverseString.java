3432954b77654fb2e060a79adad4a98ade36d4dd
ashwinikb <ashwini.kbasavaraju@gmail.com>
2018-10-23 22:51:33 -0700

    Reverse given string

--- src/main/java/com/github/ashwinikb/code/ReverseString.java
+++ src/main/java/com/github/ashwinikb/code/ReverseString.java
@@ -1,23 +1,28 @@
-/**
- * 
- */
-package com.github.ashwinikb.code;
+package com.example.ashwinikb;
 
-/**
- * @author ashwini
- *
- */
 public class ReverseString {
-	public static void main(String[] args) {
-		ReverseString.reverse("Hello");
-	}
+
 	public static String reverse(String s) {
-		String[] str = s.split("");
-		for(int i =str.length-1; i>=0;i--) {
-			System.out.print(str[i]);
+		String rev = "";
+		for (int i = s.length() - 1; i >= 0; i--) {
+			rev = rev + s.charAt(i);
 		}
-		return s;
-		
+		return rev;
+
+	}
+
+	public static String reverseSen(String s) {
+		String[] str = s.split(" ");	
+		String st = null;
+		for (int i = str.length - 1; i >= 0; i--) {
+			if (st != null) {
+				st = st + " " + str[i];
+			} else {
+				st = str[i];
+			}
+		}
+		return st;
+
 	}
 
 }
